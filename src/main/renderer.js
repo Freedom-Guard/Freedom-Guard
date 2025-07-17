c5139adff0f902e249d0ab4b688236d02f9d7dd2
Freedom-Guard-Net <118625619+Freedom-Guard-Net@users.noreply.github.com>
2025-07-17 21:55:39 +0330

    Update 8.2.1 (Starting)

--- src/main/renderer.js
+++ src/main/renderer.js
@@ -5,7 +5,7 @@ const $ = require('jquery');
 require("jquery.easing");
 const { exec, execFile, spawn } = require('child_process');
 window.$ = $;
-const vesrionApp = "8.2.0";
+const vesrionApp = "8.2.1";
 let LOGS = [];
 // #endregion
 // #region components
